e013e3ce1449ec31fba7112069d49a1307986c86
Kevin Anderson <kcanderson1969@gmail.com>
2015-06-22 22:58:27 -0500

    got it working

---
+++ application.js
@@ -0,0 +1,12 @@
+// alert("Hello World");
+function processForm(){
+    var name;
+    var message;
+    var content;
+    name = document.getElementById("name");
+    message = document.getElementById("message");
+    content = document.getElementById("output")
+  //  alert("Hello " + name.value + " your message is: " + message.value);
+
+  content.innerText = "Hello " + name.value + " your message is: " + message.value;
+}
